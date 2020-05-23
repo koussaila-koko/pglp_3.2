19170bf8de7adea7a2bd44587ec637b325f2e8a9
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-23 21:10:35 +0200

    Update vendeur.java
    checkstyle

--- 21708196/src/main/java/uvsq/pglp/vendeur.java
+++ 21708196/src/main/java/uvsq/pglp/vendeur.java
@@ -1,20 +1,34 @@
 package uvsq.pglp;
 
-public class vendeur extends employe  {
+/**
+ * la classe vendeur.
+ * 
+ * @author koussiala HAMMOUCHE .
+ */
+public class vendeur extends employe {
+	/**
+	 * commission .
+	 */
 	private int commission;
-	
-	public vendeur(int id , int salaire , int encienter, int commission) {
+
+	/**
+	 * le constroctor vendeur .
+	 * @param id         de vendeur .
+	 * @param salaire    .
+	 * @param encienter  .
+	 * @param commission .
+	 */
+	public vendeur(int id, int salaire, int encienter, int commission) {
 		super(id, salaire, encienter);
-		this.commission = commission ; 
-		
-		
-		
-	}
-	@Override
-	public int  salaire_Calcul() {
-	return super.salaire_Calcul()+commission ;
-	
-		
+		this.commission = commission;
 	}
 
+	@Override
+	/**
+	 * methede qui calcul le salaire, avec la commission .
+	 * @return le salaire plus la commission
+	 */
+	public int salaire_Calcul() {
+		return super.salaire_Calcul() + commission;
+	}
 }
