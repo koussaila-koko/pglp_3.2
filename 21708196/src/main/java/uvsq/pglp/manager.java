bb942fd3b7213c03f0c36577fcb6acea4311e533
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-23 21:10:42 +0200

    Update manager.java
    checkstyle

--- 21708196/src/main/java/uvsq/pglp/manager.java
+++ 21708196/src/main/java/uvsq/pglp/manager.java
@@ -1,17 +1,33 @@
 package uvsq.pglp;
 
+/**
+ * la classe manager.
+ * @author koussiala HAMMOUCHE .
+ */
 public class manager extends employe {
+	/**
+	 * nbSousorder c'est le nombre, sous order de manager.
+	 */
 	private int nbSousorder;
-		public manager(int id, int salaire, int encienter,int nbSousorder) {
+
+	/**
+	 * le constroctor de manager .
+	 * @param id          de manager .
+	 * @param salaire     .
+	 * @param encienter   .
+	 * @param nbSousorder .
+	 */
+	public manager(int id, int salaire, int encienter, int nbSousorder) {
 		super(id, salaire, encienter);
-		this.nbSousorder=nbSousorder;
-		// TODO Auto-generated constructor stub
+		this.nbSousorder = nbSousorder;
 	}
 
-		
-		
-		@Override
-		public int  salaire_Calcul() {
-			return super.salaire_Calcul() + nbSousorder * 100;
-		}
+	@Override
+	/**
+	 * methede qui calcul le salaire, avec la commission .
+	 * @return le salaire plus la commission
+	 */
+	public int salaire_Calcul() {
+		return super.salaire_Calcul() + nbSousorder * 100;
+	}
 }
