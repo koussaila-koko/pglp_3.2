328055ed09f7332b35c24d5fb33639c44f8b70f8
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-23 21:11:24 +0200

    Update App.java
    checkstyle

--- 21708196/src/main/java/uvsq/pglp/App.java
+++ 21708196/src/main/java/uvsq/pglp/App.java
@@ -1,13 +1,10 @@
 package uvsq.pglp;
-
 /**
- * Hello world!
- *
+ * la classe app.
+ * @author koussiala HAMMOUCHE .
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
